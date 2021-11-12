ac12d2c86ab936ebe40009a98303d79b5c13f4d9
Marta Nara <marta.kos1230@gmail.com>
2021-11-12 21:50:12 +0100

    Add subpages functionality

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,61 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(thisApp.pages[pageMatchingHash].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
   initMenu: function(){
     const thisApp = this;
 
     for(let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, rfemove from non-matching */
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, rfemove from non-matching */
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
     }
   },
   initData: function(){
@@ -46,6 +96,8 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    thisApp.initPages();
 
     thisApp.initData();
     thisApp.initCart();
