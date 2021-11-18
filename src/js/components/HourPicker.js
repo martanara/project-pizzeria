80e1c0ece4126be9ccca826bffc6f53005f61478
Marta Nara <marta.kos1230@gmail.com>
2021-11-18 16:04:39 +0100

    Fix import paths in HourPicker and DatePicker files

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,6 +1,6 @@
-import BaseWidget from '../Components/BaseWidget.js';
-import {select, settings} from '../settings.js';
-import utils from '../utils.js';
+import BaseWidget from './BaseWidget.js';
+import utils from './../utils.js';
+import {select, settings} from './../settings.js';
 
 class HourPicker extends BaseWidget{
   constructor(wrapper){
